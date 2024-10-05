5338b521fe3594c073db97ed76f82feba482c885
jonathanplin <jonathan3.lin@torontomu.ca>
2024-10-05 19:16:11 -0400

    x

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var displayName = document.getElementById("displayName");
     nameButton.onclick = function() {
     var userName = prompt("Please enter your name:");
     if (userName) {
-        displayName.innerText = "Hello, " + userName + "!";
+        displayName.innerText = "Welcome to my page, " + userName + "!";
     }
 };
 
