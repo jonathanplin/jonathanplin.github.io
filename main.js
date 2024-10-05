19b265db71bb5fa34acb86261319ed9fb5c0fb00
jonathanplin <jonathan3.lin@torontomu.ca>
2024-10-05 19:36:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -35,19 +35,20 @@ var ul = document.createElement('ul');
 // Create each navigation item
 navItems.forEach(function(item) {
     var li = document.createElement('li');
-
+    
     // Create anchor element
     var a = document.createElement('a');
     a.href = item.link; // Set the link
     a.textContent = item.name; // Set the text
-
+    
     // Append the anchor to the list item
     li.appendChild(a);
-
+    
     // Handle dropdown menu for "Portfolio"
     if (item.subItems) {
         var submenu = document.createElement('ul'); // Create a new UL for the submenu
-
+        submenu.style.display = 'none'; // Initially hide the submenu
+        
         // Submenu items
         item.subItems.forEach(function(subItem) {
             var subLi = document.createElement('li');
@@ -57,23 +58,34 @@ navItems.forEach(function(item) {
             subLi.appendChild(subA);
             submenu.appendChild(subLi);
         });
-
+        
         // Append the submenu to the "Portfolio" list item
         li.appendChild(submenu);
-
+        
         // Show submenu on mouse over
         li.onmouseover = function() {
             submenu.style.display = 'block';
         };
-
+        
         // Hide submenu on mouse out
         li.onmouseout = function() {
             submenu.style.display = 'none';
         };
     }
-
+    
     // Append the list item to the unordered list
     ul.appendChild(li);
+});
+
+// Set the display property for the main navigation
+ul.style.listStyleType = 'none'; // Remove default bullets
+ul.style.padding = '0'; // Remove default padding
+ul.style.margin = '0'; // Remove default margin
+
+// Make the main navigation items display inline (horizontally)
+ul.childNodes.forEach(function(li) {
+    li.style.display = 'inline'; // Set each list item to display inline
+    li.style.marginRight = '20px'; // Add some spacing between items
 });
 
 // Append the unordered list to the navigation bar
