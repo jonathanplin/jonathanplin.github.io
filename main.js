df799cda03f1fbab61f42a4fee71af935d10c026
jonathanplin <jonathan3.lin@torontomu.ca>
2024-10-05 18:09:24 -0400

    x

---
+++ main.js
@@ -0,0 +1,8 @@
+function mOver(some_tag) {
+    some_tag.innerHTML = "Thank You";
+     some_tag.style.color = "blue";
+  }
+  function mOut(some_tag) {
+     some_tag.innerHTML = "Mouse Over Me";
+     some_tag.style.color = "orange";
+  }
