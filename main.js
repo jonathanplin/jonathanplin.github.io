21b2c7316292fccbb2b9eb2b75d6720a12741a81
jonathanplin <jonathan3.lin@torontomu.ca>
2024-10-05 18:19:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+var nameButton = document.getElementById("nameButton");
+var displayName = document.getElementById("displayName");
 document.getElementById("nameButton").onclick = function() {
-    const userName = prompt("Please enter your name:");
+    var userName = prompt("Please enter your name:");
     if (userName) {
         document.getElementById("displayName").innerText = "Hello, " + userName + "!";
     }
